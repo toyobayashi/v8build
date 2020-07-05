2bd23a24db415fa1a626c075f5f85e0ad4b98360
toyobayashi <lifenglin314@outlook.com>
2020-07-05 11:29:20 +0800

    set-env Path

--- get_depot_tools.bat
+++ get_depot_tools.bat
@@ -2,6 +2,7 @@
 
 if not exist depot_tools git.exe clone https://chromium.googlesource.com/chromium/tools/depot_tools.git 
 set Path=%Path%;%~dp0depot_tools
+echo ::set-env name=Path::%Path%
 cd .\depot_tools
 git.exe pull
 cd ..
